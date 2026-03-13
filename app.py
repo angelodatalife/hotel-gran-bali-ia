d169ee353c85f98033dc8a0f73b189e75a72d96f
angelodatalife <angelvergaragarcia@gmail.com>
2026-03-13 12:04:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1349,7 +1349,7 @@ elif st.session_state.archivo_cargado and selected == "🧹 Camarera":
     green_bg = """
     <style>
     .stApp {
-        background-color: #1E88E5;  /* Verde pastel */
+        background-color: #663399;  /* Verde pastel */
         background-image: none;
     }
     </style>
