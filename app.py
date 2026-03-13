fccb126c75855cad019ae696d7b3b3aa896d2b02
angelodatalife <angelvergaragarcia@gmail.com>
2026-03-13 01:40:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -695,7 +695,7 @@ def mostrar_pantalla_inicio():
         """
         <h2 style='text-align: center; text-shadow: 2px 2px 4px rgba(0,0,0,0.5); font-size: 3rem; color: white; margin-top: 5px; margin-top: 200px;'>
             Check it Out! <br>
-            <span style="font-size: 0.5em; display: block;">Hotel Clean App (IA)</span>
+            <span style="font-size: 0.5em; >Hotel Clean App (IA)</span>
         </h2>
         """,
         unsafe_allow_html=True
